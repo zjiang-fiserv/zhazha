72faaf40dc44db845ef191fa7d63b6ed5381af1a
Jiang <zhou.jiang@fiserv.com>
2023-11-13 00:00:02 -0500

    add customer model, changed name, address, and number to have a customer prefix so the schema.sql does not pick up name, number, and address as keywords

--- zha/src/main/java/com/zhazha/zha/model/Customer.java
+++ zha/src/main/java/com/zhazha/zha/model/Customer.java
@@ -1,12 +1,66 @@
 package com.zhazha.zha.model;
 
+// @Entity
 public class Customer {
-    private int phoneNumber;
-    private String address;
+    private int customerNumber;
+    private String customerAddress;
     private int zip;
-    private String name;
+    private String customerName;
+
+    // @ManyToOne
+    private CustomerOrder customerOrder;
+
 
     public Customer() {
-        
+
     }
+
+    public Customer(int customerNumber, String customerAddress, int zip, String customerName) {
+        this.customerNumber = customerNumber;
+        this.customerAddress = customerAddress;
+        this.zip = zip;
+        this.customerName = customerName;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setcustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public CustomerOrder getCustomerOrder() {
+        return customerOrder;
+    }
+
+    public void setCustomerOrder(CustomerOrder customerOrder) {
+        this.customerOrder = customerOrder;
+    }
+
+    
 }
