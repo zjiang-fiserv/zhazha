fae3044b26cd96fc07e8e6e2f268a25e56047d90
Jiang <zhou.jiang@fiserv.com>
2023-11-15 00:20:08 -0500

    add create customer order table to schema.sql

--- zha/src/main/resources/schema.sql
+++ zha/src/main/resources/schema.sql
@@ -1,3 +1,8 @@
 CREATE TABLE IF NOT EXISTS CUSTOMER (customer_number INT NOT NULL PRIMARY KEY, customer_address
 VARCHAR(100), customer_name VARCHAR(50), zip int);
 
+
+
+
+CREATE TABLE IF NOT EXISTS CUSTOMER_ORDER (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+employee_id int, customer_id int, date_time TIMESTAMP)
