8500d97faa320d0da7cdf1faa257f87bc69982a7
Jiang <zhou.jiang@fiserv.com>
2023-11-15 18:49:08 -0500

    add foreign key to customer order table referencing employee id and customer number, also formatted sql statements.

--- zha/src/main/resources/schema.sql
+++ zha/src/main/resources/schema.sql
@@ -1,12 +1,28 @@
-CREATE TABLE IF NOT EXISTS CUSTOMER (customer_number INT NOT NULL PRIMARY KEY, customer_address
-VARCHAR(100), customer_name VARCHAR(50), zip int);
-
-CREATE TABLE IF NOT EXISTS CUSTOMER_ORDER (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-employee_id int, customer_number int, date_time TIMESTAMP);
-
-CREATE TABLE IF NOT EXISTS PRODUCT(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-product_name VARCHAR(50), product_description VARCHAR(255), price float);
-
-CREATE TABLE IF NOT EXISTS EMPLOYEE (id INT NOT NULL AUTO_INCREMENT, employee_name VARCHAR(50),
-employment_status VARCHAR(50), pin INT, employee_role VARCHAR(50), PRIMARY KEY (id));
-
+CREATE TABLE IF NOT EXISTS CUSTOMER (
+    customer_number INT NOT NULL PRIMARY KEY,
+    customer_address VARCHAR(100),
+    customer_name VARCHAR(50),
+    zip int
+);
+CREATE TABLE IF NOT EXISTS EMPLOYEE (
+    id INT NOT NULL AUTO_INCREMENT,
+    employee_name VARCHAR(50),
+    employment_status VARCHAR(50),
+    pin INT,
+    employee_role VARCHAR(50),
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS PRODUCT(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_description VARCHAR(255),
+    price float
+);
+CREATE TABLE IF NOT EXISTS CUSTOMER_ORDER (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_id int NOT NULL,
+    customer_number int NOT NULL,
+    date_time TIMESTAMP,
+    foreign key (employee_id) references EMPLOYEE(id),
+    foreign key (customer_number) references CUSTOMER(customer_number)
+);
