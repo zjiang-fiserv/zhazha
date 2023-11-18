b70adfd592caea95c2c36f34bda63c528e196b01
Jiang <zhou.jiang@fiserv.com>
2023-11-17 23:03:51 -0500

    fixed bugs for employee end points.

--- zha/src/main/java/com/zhazha/zha/controller/EmployeeController.java
+++ zha/src/main/java/com/zhazha/zha/controller/EmployeeController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.zhazha.zha.model.Customer;
 import com.zhazha.zha.model.Employee;
 import com.zhazha.zha.service.EmployeeService;
 import reactor.core.publisher.Flux;
@@ -29,43 +27,38 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     @ResponseStatus(HttpStatus.OK)
-    public Flux<Employee> getAllEmployees() {
-        return employeeService.findAll();
+    public Flux<Employee> getAllEmployees(@RequestParam(required = false) String employmentStatus, String role) {
+        if (employmentStatus != null)
+            return employeeService.findByStatus(employmentStatus);
+        else if (role != null)
+            return employeeService.findByRole(role);
+        else 
+            return employeeService.findAll();
     }
 
-    @GetMapping("/employees/{number}")
+    @GetMapping("/employees/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Mono<Employee> getEmployeeById(@PathVariable("number") int number) {
-        return employeeService.findByEmployeeId(number);
+    public Mono<Employee> getEmployeeById(@PathVariable("id") int id) {
+        return employeeService.findByEmployeeId(id);
     }
 
     @PostMapping("/employees")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Employee> createEmployee(@RequestBody Employee employee) {
-            return employeeService.save(new Employee(employee.getEmployeeName(), employee.getEmploymentStatus(), employee.getPin(), employee.getEmployeeRole()));
-  }
+        return employeeService.save(new Employee(employee.getEmployeeName(), employee.getEmploymentStatus(),
+                employee.getPin(), employee.getEmployeeRole()));
+    }
 
-    // @PutMapping("/employees/{number}")
-    // @ResponseStatus(HttpStatus.OK)
-    // public Mono<Employee> updateEmployee(@PathVariable("number") int number, @RequestBody Employee employee) {
-    //     return employeeService.update(number, employee.getEmploymentStatus(), employee.getEmployeeName(), employee.getEmployeeRole(), employee);
-    // }
+    @PutMapping("/employees/{number}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Employee> updateEmployee(@PathVariable("number") int number,
+            @RequestBody Employee employee) {
+        return employeeService.update(number, employee);
+    }
 
     @DeleteMapping("/employees/{number}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteEmployee(@PathVariable("number") int number) {
         return employeeService.deleteByID(number);
-    }
-
-    @GetMapping("/employees/status/{status}")
-    @ResponseStatus(HttpStatus.OK)
-    public Flux<Employee> findByStatus(String status) {
-        return employeeService.findByStatus(status);
-    }
-
-    @GetMapping("/employees/roles/{role}")
-    @ResponseStatus(HttpStatus.OK)
-    public Flux<Employee> findByRole(String role) {
-        return employeeService.findByRole(role);
     }
 }
